631256f898132533f8e39572405ed2a5dda36ea8
boris <nikiforov5000@gmail.com>
2023-01-16 21:17:24 +0300

    added constants for empty sizedBox

--- lib/constants/text_styles.dart
+++ lib/constants/text_styles.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
+
 const kProductScreenTitleTextStyle = TextStyle(
   fontWeight: FontWeight.normal,
   fontSize: 20.0,
 );
+
+const kProductScreenRatingTextStyle = TextStyle(
+    fontSize: 20.0,);
+
+
+getRatingColor(double rating) {
+  if (rating > 4.0) {
+    return Colors.green;
+  }
+  if (rating > 3.0) {
+    return Colors.yellow;
+  }
+  if (rating > 2.0) {
+    return Colors.orange;
+  }
+  if (rating > 1.0) {
+    return Colors.red;
+  }
+  return Colors.transparent;
+}
--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../constants/text_styles.dart';
 import '../models/product.dart';
+import '../widgets/sized_box_vertical_separator.dart';
 
 class ProductScreen extends StatelessWidget {
   const ProductScreen({super.key});
@@ -16,30 +17,49 @@ class ProductScreen extends StatelessWidget {
         title: Text(product.title),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 40.0),
-        child: Column(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 50.0,
+        ),
+        child: ListView(
           children: [
+            kProductScreenTopBottomBlancSizedBox,
             Image(image: NetworkImage(product.imgUrl)),
-            SizedBox(height: 30.0,),
-            Text(product.title, style: kProductScreenTitleTextStyle, textAlign: TextAlign.center,),
-            Text(product.rate.toString(), style: TextStyle(backgroundColor: getRatingColor(product.rate)),),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              product.title,
+              style: kProductScreenTitleTextStyle,
+              textAlign: TextAlign.center,
+            ),
+            kProductScreenColumnSizedBox,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Rating: ',
+                  style: kProductScreenRatingTextStyle,
+                ),
+                Container(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: getRatingColor(product.rate),
+                  ),
+                  child: Text(
+                    product.rate.toString(),
+                    style: kProductScreenRatingTextStyle,
+                  ),
+                ),
+              ],
+            ),
+            kProductScreenColumnSizedBox,
             Text(product.discription),
+            kProductScreenTopBottomBlancSizedBox,
           ],
         ),
       ),
     );
-  }
-
-  getRatingColor(double rating) {
-    if (rating > 4.0) {
-      return Colors.green;
-    }
-    if (rating > 3.0) {
-      return Colors.yellow;
-    }
-    if (rating > 2.0) {
-      return Colors.orange;
-    }
-    return Colors.red;
   }
 }
