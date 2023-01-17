55f45f6a6604f06a56e7fa405ff1d9a4e19858b0
boris <nikiforov5000@gmail.com>
2023-01-17 13:59:40 +0300

    added horizontal listview above products with fake buttons

--- lib/screens/products_list_screen.dart
+++ lib/screens/products_list_screen.dart
@@ -1,8 +1,8 @@
 import 'package:ecommerce_demo/models/product_data.dart';
 import 'package:ecommerce_demo/screens/product_screen.dart';
+import 'package:ecommerce_demo/widgets/sized_box_vertical_separator.dart';
 import 'package:flutter/material.dart';
 
-import '../models/product.dart';
 import '../widgets/bottom_navbar.dart';
 import '../widgets/product_tile.dart';
 
@@ -19,22 +19,56 @@ class _ProductsListScreenState extends State<ProductsListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.count(
-        crossAxisCount: 3,
-        children: List.generate(
-          productData.getLength(),
-          // (index) => Container(child: Text(index.toString()),),
-          (index) => ProductTile(
-            product: productData.getProduct(index),
-            onTapCallback: () {
-              Navigator.pushNamed(context, ProductScreen.id, arguments: productData.getProduct(index));
-            },
+      body: Column(
+        children: [
+          kProductScreenTopBottomBlancSizedBox,
+          Expanded(
+            flex: 1,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: List.generate(
+                5,
+                (index) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10)),
+                    height: 50,
+                    width: 100,
+                    child: InkWell(
+                      onTap: () { print('Category'); },
+                      child: Center(
+                        child: Text('Category'),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
-        ),
+          Expanded(
+            flex: 9,
+            child: GridView.count(
+              // shrinkWrap: true,
+              // physics: NeverScrollableScrollPhysics(),
+              crossAxisCount: 3,
+              children: List.generate(
+                productData.getLength(),
+                // (index) => Container(child: Text(index.toString()),),
+                (index) => ProductTile(
+                  product: productData.getProduct(index),
+                  onTapCallback: () {
+                    Navigator.pushNamed(context, ProductScreen.id,
+                        arguments: productData.getProduct(index));
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
       bottomNavigationBar: NavBar(),
     );
   }
 }
-
-
