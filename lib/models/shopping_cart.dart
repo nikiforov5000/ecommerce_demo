a85a2730030955b921aae980b08d4c1ea0f0b0c3
boris <nikiforov5000@gmail.com>
2023-01-26 12:29:43 +0400

    fixed wrong increasing sum

--- lib/models/shopping_cart.dart
+++ lib/models/shopping_cart.dart
@@ -2,7 +2,6 @@ import 'package:ecommerce_demo/models/product.dart';
 
 ShoppingCart shoppingCart = ShoppingCart();
 
-
 class ShoppingCart {
   Map<Product, int> _shoppingCart = {};
   double sum = 0;
@@ -20,16 +19,10 @@ class ShoppingCart {
   }
 
   addProduct(Product product, int quantity) {
+    sum += product.price * quantity;
     if (_shoppingCart.keys.contains(product)) {
-      print('contains');
-      int? currentQuantity = _shoppingCart[product];
-      _shoppingCart[product] = quantity + currentQuantity!;
+      quantity += _shoppingCart[product]!;
     }
-    else {
-      print('doesnt contains');
-      _shoppingCart[product] = quantity;
-      sum += product.price * quantity;
-    }
+    _shoppingCart[product] = quantity;
   }
-
 }
