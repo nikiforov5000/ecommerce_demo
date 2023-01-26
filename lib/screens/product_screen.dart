14b907e3680ca94734e0d4d318bed7b69adb0a36
boris <nikiforov5000@gmail.com>
2023-01-26 16:18:09 +0400

    splited into blocks

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -8,21 +8,14 @@ import '../models/product.dart';
 import '../models/shopping_cart.dart';
 import '../widgets/sized_box_vertical_separator.dart';
 
-class ProductScreen extends StatefulWidget {
-  const ProductScreen({super.key});
-
+class ProductScreen extends StatelessWidget {
   static const String id = '/productScreen';
 
   @override
-  State<ProductScreen> createState() => _ProductScreenState();
-}
-
-class _ProductScreenState extends State<ProductScreen> {
-  int quantity = 1;
-
-  @override
   Widget build(BuildContext context) {
-    print('productScreen().build');
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
     final product = ModalRoute.of(context)!.settings.arguments as Product;
     return Scaffold(
       appBar: AppBar(
@@ -30,109 +23,24 @@ class _ProductScreenState extends State<ProductScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(
-          horizontal: 50.0,
+          horizontal: 20.0,
         ),
-        child: ListView(
+        child: Column(
           children: [
-            // Image(image: NetworkImage(product.imgUrl)),
+            SizedBox(
+              height: screenHeight * .03,
+            ),
             ProductImageCarousel(
               product: product,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: RoundedButton(
-                    color: Colors.blue,
-                    text: '-', //2212
-                    onPress: () {
-                      setState(() {
-                        if (quantity > 1) {
-                          quantity--;
-                        }
-                      });
-                    },
-                  ),
-                ),
-                // SizedBox(
-                //   width: 30,
-                // ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                  ),
-                  child: Text(
-                    quantity.toString(),
-                    style: TextStyle(fontSize: 40),
-                  ),
-                ),
-                // SizedBox(
-                //   width: 30,
-                // ),
-                Expanded(
-                  child: RoundedButton(
-                    color: Colors.blue,
-                    text: '+', //002b
-                    onPress: () {
-                      setState(() {
-                        quantity++;
-                      });
-                    },
-                  ),
-                ),
-              ],
-            ),
             SizedBox(
-              height: 30.0,
+              height: screenHeight * .03,
             ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            RoundedButton(
-                text: 'Add to Cart',
-                color: Colors.blue,
-                onPress: () {
-                  shoppingCart.addProduct(product, quantity);
-                }),
-            RoundedButton(
-                text: 'Go to Cart',
-                color: Colors.blue,
-                onPress: () {
-                  Navigator.pushNamed(context, ShoppingCartScreen.id);
-                }),
+            ButtonsBlock(product: product),
             SizedBox(
-              height: 30.0,
+              height: screenHeight * .03,
             ),
-            Text(
-              product.title,
-              style: kProductScreenTitleTextStyle,
-              textAlign: TextAlign.center,
-            ),
-            kProductScreenColumnSizedBox,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Rating: ',
-                  style: kProductScreenRatingTextStyle,
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 10.0, vertical: 5.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: getRatingColor(product.rate),
-                  ),
-                  child: Text(
-                    product.rate.toString(),
-                    style: kProductScreenRatingTextStyle,
-                  ),
-                ),
-              ],
-            ),
-            kProductScreenColumnSizedBox,
-            Text(product.discription),
-            kProductScreenTopBottomBlancSizedBox,
+            Expanded(child: AboutProduct(product: product)),
           ],
         ),
       ),
@@ -148,22 +56,142 @@ class ProductImageCarousel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CarouselSlider(
-      options: CarouselOptions(height: 400.0),
+      options: CarouselOptions(height: 200.0),
       items: [1, 2, 3, 4, 5].map((i) {
         return Builder(
           builder: (BuildContext context) {
             return Container(
               width: MediaQuery.of(context).size.width,
               margin: EdgeInsets.symmetric(horizontal: 5.0),
-              // decoration: BoxDecoration(
-              //     color: Colors.amber
-              // ),
-              // child: Text('text $i', style: TextStyle(fontSize: 16.0),)
               child: Image(image: NetworkImage(product.imgUrl)),
             );
           },
         );
       }).toList(),
+    );
+  }
+}
+
+class ButtonsBlock extends StatefulWidget {
+  const ButtonsBlock({required this.product});
+
+  final Product product;
+
+  @override
+  State<ButtonsBlock> createState() => _ButtonsBlockState();
+}
+
+class _ButtonsBlockState extends State<ButtonsBlock> {
+  int quantity = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: RoundedButton(
+                  color: Colors.blue,
+                  text: '-',
+                  onPress: () {
+                    setState(() {
+                      if (quantity > 1) {
+                        quantity--;
+                      }
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: Text(
+                  quantity.toString(),
+                  style: TextStyle(fontSize: 40),
+                ),
+              ),
+              Expanded(
+                child: RoundedButton(
+                  color: Colors.blue,
+                  text: '+', //002b
+                  onPress: () {
+                    setState(() {
+                      quantity++;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          RoundedButton(
+              text: 'Add to Cart',
+              color: Colors.blue,
+              onPress: () {
+                shoppingCart.addProduct(widget.product, quantity);
+              }),
+          SizedBox(
+            height: 15,
+          ),
+          RoundedButton(
+              text: 'Go to Cart',
+              color: Colors.blue,
+              onPress: () {
+                Navigator.pushNamed(context, ShoppingCartScreen.id);
+              }),
+        ]);
+  }
+}
+
+class AboutProduct extends StatelessWidget {
+  const AboutProduct({required this.product});
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          product.title,
+          style: kProductScreenTitleTextStyle,
+          textAlign: TextAlign.center,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Rating: ',
+              style: kProductScreenRatingTextStyle,
+            ),
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: getRatingColor(product.rate),
+              ),
+              child: Text(
+                product.rate.toString(),
+                style: kProductScreenRatingTextStyle,
+              ),
+            ),
+          ],
+        ),
+        Expanded(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+              child: Text(product.discription),
+          ),
+        ),
+      ],
     );
   }
 }
