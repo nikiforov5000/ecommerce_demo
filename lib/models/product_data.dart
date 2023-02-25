67cbd2690a4a706377c7fd6139ada0e71600192d
boris <nikiforov5000@gmail.com>
2023-02-25 19:17:02 +0300

    cleaned unused methods

--- lib/models/product_data.dart
+++ lib/models/product_data.dart
@@ -21,47 +21,6 @@ class ProductData {
   static List<Product> products = [];
   static List<String> _categories = [];
 
-  // Future<bool> fetchData() async {
-  //   http.Client client = http.Client();
-  //   String url = 'https://fakestoreapi.com/products';
-  //   var uri = Uri.parse(url);
-  //   try {
-  //     var response = await client.get(uri);
-  //     if (response.statusCode == 200 && response.body.length > 10) {
-  //       var decodedResponse = jsonDecode(response.body);
-  //       fillProductList(decodedResponse);
-  //     } else {
-  //       print('response BAD');
-  //       print(response.statusCode);
-  //     }
-  //   } catch (e) {
-  //     print('uri ' + uri.toString());
-  //     print('product_data.dart -> fetchData() error:' + e.toString());
-  //   }
-  //   return true;
-  // }
-
-  // void fillProductList(decodedResponse) {
-  //   int k = 0;
-  //   for (int i = 0; i < 20; ++i) {
-  //     var response = decodedResponse[i];
-  //
-  //     String category = response['category'];
-  //     category = category.capitalize();
-  //
-  //     _categories.putIfAbsent(category, () => k++);
-  //
-  //     _products.add(Product(
-  //       title: response['title'],
-  //       price: response['price'].toDouble(),
-  //       imgUrl: response['image'],
-  //       description: response['description'],
-  //       rate: response['rating']['rate'].toDouble(),
-  //       category: category,
-  //     ));
-  //   }
-  // }
-
   getCategoriesList() {
     return _categories;
   }
@@ -84,8 +43,10 @@ class ProductData {
 
   static getProductsOfCategory(String category) async {
     List<Product> products = [];
-    QuerySnapshot<Map<String, dynamic>> snapshot =
-        await _firestore.collection('products').where('category', isEqualTo: category).get();
+    QuerySnapshot<Map<String, dynamic>> snapshot = await _firestore
+        .collection('products')
+        .where('category', isEqualTo: category)
+        .get();
     snapshot.docs.forEach((document) {
       products.add(Product.buildFromMap(document));
     });
