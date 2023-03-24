613fafa328202d591d9d151eb05e1c8dbb040895
boris <nikiforov5000@gmail.com>
2023-03-24 10:16:50 +0300

    display uid and email on user page

--- lib/screens/user_account_screen.dart
+++ lib/screens/user_account_screen.dart
@@ -1,4 +1,6 @@
+import 'package:ecommerce_demo/services/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UserAccountScreen extends StatelessWidget {
   static String id = 'user_account_screen';
@@ -7,8 +9,22 @@ class UserAccountScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // final _authService = Provider.of<AuthService>(context);
+    // final _authUser = _authService.user.first.then((value) => value!.uid);
+    final _userProvider = Provider.of<UserProvider>(context);
+    final user = _userProvider.user;
     return Scaffold(
-      body: const Placeholder(),
+      body: user != null
+          ? Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(user.uid),
+                  Text(user.email),
+                ],
+              ),
+          )
+          : Text('user is null'),
     );
   }
 }
