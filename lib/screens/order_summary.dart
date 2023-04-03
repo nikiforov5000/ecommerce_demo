2c7d798a6b46357490e8f6dcf033140297f8861c
boris <nikiforov5000@gmail.com>
2023-04-03 13:53:36 +0300

    visual content only, no backend

--- lib/screens/order_summary.dart
+++ lib/screens/order_summary.dart
@@ -1,120 +1,316 @@
-import 'package:ecommerce_demo/constants/colors.dart';
-import 'package:ecommerce_demo/constants/shadows.dart';
-import 'package:ecommerce_demo/models/product.dart';
 import 'package:ecommerce_demo/models/shopping_cart.dart';
-import 'package:ecommerce_demo/screens/checkout_screen.dart';
-import 'package:ecommerce_demo/services/user_provider.dart';
-import 'package:ecommerce_demo/widgets/bank_card.dart';
 import 'package:ecommerce_demo/widgets/logout_button.dart';
 import 'package:ecommerce_demo/widgets/user_avatar.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class OrderSummary extends StatelessWidget {
   static const String id = 'order_summary';
 
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height;
-    final _userProvider = Provider.of<UserProvider>(context);
-    final _user = _userProvider.user;
+    final totalSum = shoppingCart.getSum();
+
     return Scaffold(
       appBar: AppBar(
+        title: Text('Order Summary'),
         actions: [
           UserAvatarWidget(),
           LogoutButton(),
         ],
       ),
-      body: Container(
-        // color: kBackgroundColor,
-        child: Column(
-          children: [
-            Flexible(
-              flex: 10,
-              child: Container(
-                child: ListView(
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children:
-                        List.generate(shoppingCart.length, (index) {
-                          final product = shoppingCart.getCartMap().keys.toList()[index];
-                          final int amount = shoppingCart.getCartMap().values.toList()[index];
-                          final String priceDescription = amount.toString() + ' x ' + product.price.toString();
-                          return Container(
-                            decoration: BoxDecoration(
-                              // color: kBackgroundColor,
-                              // boxShadow: k3DShadows,
-                            ),
-                            height: height * 0.05,
-                            child: Row(
+      body: SafeArea(
+        child: Container(
+          color: Colors.white,
+          padding: const EdgeInsets.only(
+              left: 10.0, right: 10.0, top: 10.0, bottom: 20.0),
+          child: Column(
+            children: [
+              Flexible(
+                child: ListView(children: [
+                  Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Please confirm and submit your order',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'By clicking submit order, you agree to eCommerce Demo'
+                            ' Terms of Use and Privacy Policy',
+                        style: TextStyle(
+                            fontSize: 14, color: Colors.black54, height: 1.5),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Card(
+                    elevation: 2,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 30.0),
+                        child: Column(
+                          children: [
+                            Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                Flexible(child: Image.network(product.imgUrl)),
-                                Flexible(child: Text(product.title)),
-                                Flexible(child: Text(priceDescription)),
+                                Text(
+                                  'Payment',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                GestureDetector(
+                                  onTap: () {},
+                                  child: Text(
+                                    'Edit',
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.teal),
+                                  ),
+                                ),
                               ],
                             ),
-                          );
-                        }),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(Icons.credit_card),
+                                    SizedBox(
+                                      width: 10.0,
+                                    ),
+                                    Text(
+                                      '••••',
+                                      style: TextStyle(
+                                          fontSize: 16, color: Colors.black54),
+                                    ),
+                                    SizedBox(
+                                      width: 10.0,
+                                    ),
+                                    Text(
+                                      '9643',
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  '11/25',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.black54),
+                                ),
+                              ],
+                            ),
+                          ],
+                        )),
+                  ),
+                  Card(
+                    elevation: 2,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 30.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Shipping address',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                GestureDetector(
+                                  onTap: () {},
+                                  child: Text(
+                                    'Edit',
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.teal),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Name',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.black54),
+                                ),
+                                Text(
+                                  'Boris Nikiforov',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Street',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.black54),
+                                ),
+                                Text(
+                                  'Moda Cd. No:10',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'ZIP',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.black54),
+                                ),
+                                Text(
+                                  '34710',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        )),
+                  ),
+                ]),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 20.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.grey.shade100,
                     ),
-                  ],
-                ),
-              ),
-            ),
-            Flexible(
-              flex: 2,
-              child: Container(
-                color: Colors.blueGrey,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text('Total: \$' + shoppingCart.getSum().toStringAsFixed(2), style: TextStyle(fontWeight: FontWeight.bold),),
-                  ],
-                ),
-              ),
-            ),
-            Flexible(
-              flex: 5,
-              child: Container(
-                color: Colors.greenAccent,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text('Your details:'),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Address:'),
-                        Text('Caferağa, Moda Cd. No:10, 34710 Kadıköy/İstanbul'),
-                      ],
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 30.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Order Summary',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Subtotal',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.black54),
+                                ),
+                                Text(
+                                  '\$${totalSum * 0.9}',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Delivery',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.black54),
+                                ),
+                                Text(
+                                  '\$${totalSum * 0.1}',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Total',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  '\$$totalSum',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ],
+                        )),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      child: Text('Submit Order'),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ),
-            Flexible(
-              flex: 10,
-              child: Container(
-                color: Colors.purpleAccent.shade100,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text('Payment method details:'),
-                    BankCard(),
-                  ],
-                ),
-              ),
-            ),
-            Flexible(
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Pay'),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
