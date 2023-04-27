a75427d8feee4307af2291ce2ebc31b46f324b76
boris <nikiforov5000@gmail.com>
2023-04-27 16:48:25 +0300

    added localUser in wrapper

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -4,6 +4,7 @@ import 'package:ecommerce_demo/models/user_account/user_account.dart';
 import 'package:ecommerce_demo/screens/categories_screen/categories_screen.dart';
 import 'package:ecommerce_demo/screens/login/login_screen.dart';
 import 'package:ecommerce_demo/services/auth_service.dart';
+import 'package:ecommerce_demo/services/local_user_provider.dart';
 import 'package:ecommerce_demo/services/shopping_cart_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -21,6 +22,7 @@ class Wrapper extends StatelessWidget {
   Widget build(BuildContext context) {
     final authService = Provider.of<AuthService>(context);
     final shoppingCartProvider = Provider.of<ShoppingCartProvider>(context);
+    final userProvider = Provider.of<LocalUserProvider>(context);
 
     return StreamBuilder<LocalUser?>(
       stream: authService.user,
@@ -33,6 +35,7 @@ class Wrapper extends StatelessWidget {
           }
           else {
             print('wrapper.dart -> user is ${user.email}');
+            userProvider.localUser = user;
             _setCurrentShoppingCart(shoppingCartProvider, user);
             return CategoriesScreen();
           }
