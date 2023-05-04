fc86ae8483d15da1dbc4c3c110a3069866f2fc79
boris <nikiforov5000@gmail.com>
2023-05-04 14:12:23 +0300

    added gesture dedector and navigator push named

--- lib/widgets/cart_icon/create_icon.dart
+++ lib/widgets/cart_icon/create_icon.dart
@@ -1,19 +1,21 @@
 import 'package:ecommerce_demo/constants/colors.dart';
-import 'package:ecommerce_demo/widgets/cart_icon/small_number_icon.dart';
 import 'package:flutter/material.dart';
 
 class CreateIcon extends StatelessWidget {
-  final int _amount;
   final TextStyle style = const TextStyle(color: Colors.black);
 
-  const CreateIcon({super.key, required amount}) : _amount = amount;
+  const CreateIcon({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       alignment: AlignmentDirectional.center,
-      children: [
-        Icon(Icons.shopping_cart, color: kAppBarIconColor, size: 23,),
+      children: const [
+        Icon(
+          Icons.shopping_cart,
+          color: kAppBarIconColor,
+          size: 23,
+        ),
       ],
     );
   }
--- lib/widgets/cart_icon/stream_builder.dart
+++ lib/widgets/cart_icon/stream_builder.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_demo/models/shopping_cart/shopping_cart.dart';
+import 'package:ecommerce_demo/screens/shopping_cart_screen/shopping_cart_screen.dart';
 import 'package:ecommerce_demo/services/shopping_cart_provider.dart';
 import 'package:ecommerce_demo/widgets/cart_icon/create_icon.dart';
 import 'package:flutter/material.dart';
@@ -9,14 +11,18 @@ class CartIconStreamBuilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final cartProvider = Provider.of<ShoppingCartProvider>(context);
-    final cart = cartProvider.shoppingCart;
+    final ShoppingCart? cart = cartProvider.shoppingCart;
     return StreamBuilder<int>(
-      stream: cart!.isNotEmptyStream(),
+      stream: cart?.isNotEmptyStream(),
       initialData: 0,
       builder: (context, snapshot) {
         if (snapshot.hasData && snapshot.data! > 0) {
-          int amount = snapshot.data!;
-          return CreateIcon(amount: amount);
+          return GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, ShoppingCartScreen.id);
+            },
+            child: const CreateIcon(),
+          );
         }
         return Container();
       },
