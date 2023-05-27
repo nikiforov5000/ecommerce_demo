03a4d5fe16dcfe2284e3e9b1e8cfd15926488c6f
boris <nikiforov5000@gmail.com>
2023-05-27 19:12:51 +0300

    refactor in process

---
+++ lib/screens/order_summary/widgets/payment_details_card.dart
@@ -0,0 +1,75 @@
+
+class PaymentDetailsCard extends StatelessWidget {
+  const PaymentDetailsCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return                   Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: 20.0, vertical: 30.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Payment',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: const Text(
+                      'Edit',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.teal),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.credit_card),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '••••',
+                        style: TextStyle(
+                            fontSize: 16, color: Colors.black54),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '9643',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    '11/25',
+                    style: TextStyle(
+                        fontSize: 16, color: Colors.black54),
+                  ),
+                ],
+              ),
+            ],
+          )),
+    );
+  }
+}
+
---
+++ lib/screens/order_summary/widgets/privacy_policy_text.dart
@@ -0,0 +1,19 @@
+
+class PrivacyPolicyText extends StatelessWidget {
+  const PrivacyPolicyText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'By clicking submit order, you agree to eCommerce Demo'
+          ' Terms of Use and Privacy Policy',
+      style: TextStyle(
+        fontSize: 14,
+        color: Colors.black54,
+        height: 1.5,
+      ),
+    );
+  }
+}
+
+
